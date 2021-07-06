cc5a54b3afb791017344b4f8359b2bf26683191c
Isadora Aguiar <isadora.ibag@gmail.com>
2021-07-06 10:55:45 -0300

    remove example code

--- python1/input.py
+++ python1/input.py
@@ -2,13 +2,6 @@ nome = input('Qual é o seu nome?')
 idade = input('Qual a sua idade?')
 altura = input('Qual a sua aultura?')
 sobrenome = input('Qual a seu sobrenome?') 
-# print(nome, idade, altura, sobrenome)
-
-
-salvar_no_banco({
-    'nome': nome,
-    'idade': idade,
-    'altura': altura,
-    'sobrenome': sobrenome
-})
+print(nome, idade, altura, sobrenome)
+# input permite receber dados diferentes vindo de diferentes usuários
 
